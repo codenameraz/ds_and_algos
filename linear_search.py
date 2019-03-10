49066633e7ce096ad0efd8a540306d2c7c9f71c3
codenameraz <eshirazi89@gmail.com>
2019-03-09 22:08:43 -0500

    initial commit

---
+++ linear_search.py
@@ -0,0 +1,29 @@
+def linear_search(my_item, my_list):
+
+    count = 0
+    position = 0
+    found = False
+
+    while position < len(my_list) and not found:
+        if my_list[position] == my_item:
+            found = True
+        position += 1 
+        count += 1
+        print 'Current Search Count: {}'.format(count)
+
+        if my_item not in my_list:
+            my_list.append(my_item)
+
+    return found
+
+game_list = ["Devil May Cry 5", "World of Final Fantasy", "Super Mario Odyssey"]
+item = "Super Mario Odyssey"
+second_item = "Diablo 3"
+
+result = linear_search(item, game_list)
+if result:
+    print 'Found item: {}'.format(item)
+
+result_two = linear_search(second_item, game_list)
+if result_two:
+    print 'Found item: {}'.format(second_item)
